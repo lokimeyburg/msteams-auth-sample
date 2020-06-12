56c230bffb993c351b6a158755eefc8aca479013
Bob German <bgerman1@hotmail.com>
2020-06-12 17:17:08 -0400

    Improved consent experience w/inline buttons

--- src/static/scripts/ssoDemo.js
+++ src/static/scripts/ssoDemo.js
@@ -5,38 +5,55 @@
     // This button will be enabled if the on-behalf-of-flow fails
     // due to requiring further consent. It shows the user an AAD
     // consent dialog and asks for additional permission
-    function initializeConsentButton() {
-        var btn = document.getElementById("promptForConsentButton")
-        btn.onclick = () => {
+    // function initializeConsentButton() {
+    //     var btn = document.getElementById("promptForConsentButton")
+    //     btn.onclick = () => {
+    //         microsoftTeams.authentication.authenticate({
+    //             url: window.location.origin + "/auth/auth-start",
+    //             width: 600,
+    //             height: 535,
+    //             successCallback: function (result) {
+    //                 let data = localStorage.getItem(result);
+    //                 localStorage.removeItem(result);
+    //                 printLog("Success! Additional permission granted. Result: " + data)
+    //                 window.location.reload();
+    //             },
+    //             failureCallback: function (reason) {
+    //                 printLog("Failure. Additional permission was not granted. Result: " + JSON.stringify(reason))
+    //             }
+    //         });
+    //     }
+    // }
+
+    function requestConsent() {
+        return new Promise((resolve, reject) => {
             microsoftTeams.authentication.authenticate({
                 url: window.location.origin + "/auth/auth-start",
                 width: 600,
                 height: 535,
-                successCallback: function (result) {
+                successCallback: (result) => {
                     let data = localStorage.getItem(result);
                     localStorage.removeItem(result);
-                    printLog("Success! Additional permission granted. Result: " + data)
-                    window.location.reload();
+                    resolve(data);
                 },
-                failureCallback: function (reason) {
-                    printLog("Failure. Additional permission was not granted. Result: " + JSON.stringify(reason))
+                failureCallback: (reason) => {
+                    reject(JSON.stringify(reason));
                 }
             });
-
-        }
+        });
     }
 
     // 1. Get auth token
     // Ask Teams to get us a token from AAD
-    function step1_GetAuthToken() {
+    function GetClientSideToken() {
 
         return new Promise((resolve, reject) => {
 
-            printLog("1. Get auth token from Microsoft Teams");
+            display("1. Get auth token from Microsoft Teams");
 
             microsoftTeams.authentication.getAuthToken({
                 successCallback: (result) => {
-                    printLog(result)
+                    display(result)
                     resolve(result);
                 },
                 failureCallback: function (error) {
@@ -50,9 +67,9 @@
 
     // 2. Exchange that token for a token with the required permissions
     //    using the web service (see /auth/token handler in app.js)
-    function step2_ExchangeForServerSideToken(clientSideToken) {
+    function GetServerSideToken(clientSideToken) {
 
-        printLog("2. Exchange for server-side token");
+        display("2. Exchange for server-side token");
 
         return new Promise((resolve, reject) => {
 
@@ -63,32 +80,37 @@
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({ "tid": context.tid, "token": clientSideToken }),
+                    body: JSON.stringify({
+                        'tid': context.tid,
+                        'token': clientSideToken 
+                    }),
                     mode: 'cors',
                     cache: 'default'
                 })
-                .then((response) => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        reject(response.error);
-                    }
-                })
-                .then((responseJson) => {
-                    if (responseJson.error) {
-                        reject(responseJson.error);
-                    } else {
-                        const serverSideToken = responseJson;
-                        printLog(serverSideToken);
-                        resolve(serverSideToken);
-                    }
-                });
+                    .then((response) => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            reject(response.error);
+                        }
+                    })
+                    .then((responseJson) => {
+                        if (responseJson.error) {
+                            reject(responseJson.error);
+                        } else {
+                            const serverSideToken = responseJson;
+                            display(serverSideToken);
+                            resolve(serverSideToken);
+                        }
+                    });
             });
         });
     }
 
     // 3. Get the server side token and use it to call the Graph API
-    function step3_UseServerSideToken(data) {
+    function UseServerSideToken(data) {
+
+        display("3. Call https://graph.microsoft.com/v1.0/me/ with the server side token");
 
         return fetch("https://graph.microsoft.com/v1.0/me/",
             {
@@ -108,35 +130,49 @@
                 }
             })
             .then((profile) => {
-                printLog(JSON.stringify(profile, undefined, 4), 'pre');
+                display(JSON.stringify(profile, undefined, 4), 'pre');
             });
 
     }
 
     // Add text to the display in a <p> or other HTML element
-    function printLog(text, elementTag) {
+    function display(text, elementTag) {
         var logDiv = document.getElementById('logs');
         var p = document.createElement(elementTag ? elementTag : "p");
         p.innerText = text;
         logDiv.append(p);
         console.log("ssoDemo: " + text);
+        return p;
     }
 
     // In-line code
-    initializeConsentButton();
-    step1_GetAuthToken()
+    GetClientSideToken()
         .then((clientSideToken) => {
-            return step2_ExchangeForServerSideToken(clientSideToken);
+            return GetServerSideToken(clientSideToken);
         })
         .then((serverSideToken) => {
-            return step3_UseServerSideToken(serverSideToken);
+            return UseServerSideToken(serverSideToken);
         })
         .catch((error) => {
             if (error === "invalid_grant") {
-                printLog("Server needs user consent - enable the 'Grant Permission' button");
-                document.getElementById("promptForConsentButton").disabled = false;
+                display(`Error: ${error} - user or admin consent required`);
+                let button = display("Consent", "button");
+                button.onclick = (() => {
+                    requestConsent()
+                        .then((result) => {
+                            let accessToken = JSON.parse(result).accessToken;
+                            display(`Received access token ${accessToken}`);
+                            UseServerSideToken(accessToken);
+                        })
+                        .catch((error) => {
+                            display(`ERROR ${error}`);
+                            button.disabled = true;
+                            let refreshButton = display("Refresh page", "button");
+                            refreshButton.onclick = (() => { window.location.reload(); });
+                        });
+                });
             } else {
-                printLog(`Error from web service: ${error}`);
+                display(`Error from web service: ${error}`);
             }
         });
 
